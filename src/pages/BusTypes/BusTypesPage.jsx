50b6df87ffa12a3cfe60674188fb2fa76dc8af5c
basantsaed2 <basantsaed718@gmail.com>
2026-03-04 10:38:37 +0200

    ed

--- src/pages/BusTypes/BusTypesPage.jsx
+++ src/pages/BusTypes/BusTypesPage.jsx
@@ -28,7 +28,6 @@ const BusTypesPage = () => {
         return data?.data?.busTypes || data?.busTypes || [];
     }, [data]);
 
-
     return (
         <div className="space-y-6 animate-in fade-in slide-in-from-bottom-2 duration-500">
             {/* Header */}

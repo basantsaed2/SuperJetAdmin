c6ce521b0a12e1301379d9f1818905269e5fd586
olaalam <olaallam163@gmail.com>
2026-03-04 10:31:55 +0200

    edits

--- src/pages/BusTypes/BusTypesPage.jsx
+++ src/pages/BusTypes/BusTypesPage.jsx
@@ -29,7 +29,6 @@ const BusTypesPage = () => {
     }, [data]);
 
 
-    
     return (
         <div className="space-y-6 animate-in fade-in slide-in-from-bottom-2 duration-500">
             {/* Header */}

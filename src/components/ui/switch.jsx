05048c194d332514a25a59fd466289a6a3794aa2
basantsaed2 <basantsaed718@gmail.com>
2026-03-07 15:59:50 +0200

    edit switch

--- src/components/ui/switch.jsx
+++ src/components/ui/switch.jsx
@@ -1,14 +1,17 @@
 import * as React from "react"
 import { Switch as SwitchPrimitive } from "radix-ui"
-
 import { cn } from "@/lib/utils"
+import { useTranslation } from "react-i18next";
 
 function Switch({ className, ...props }) {
+  const { i18n } = useTranslation();
+  const isRtl = i18n.dir() === "rtl";
+
   return (
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors shadow-xs outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input dark:data-[state=checked]:bg-primary dark:data-[state=unchecked]:bg-input/50",
+        "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors shadow-xs outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
         className
       )}
       {...props}
@@ -16,7 +19,11 @@ function Switch({ className, ...props }) {
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          "pointer-events-none block size-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0 dark:bg-foreground"
+          "pointer-events-none block size-4 rounded-full bg-background shadow-lg ring-0 transition-transform",
+          // LTR Logic
+          !isRtl && "data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
+          // RTL Logic: Slide to the left (negative) instead of right
+          isRtl && "data-[state=checked]:-translate-x-4 data-[state=unchecked]:translate-x-0"
         )}
       />
     </SwitchPrimitive.Root>
